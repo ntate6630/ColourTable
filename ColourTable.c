34c9311c29d4003d5dfa3ef63dfc8a1341467530
Nick Tate <ntate6630@yahoo.co.uk>
2022-11-11 12:51:02 +0000

    Add files via upload

---
+++ ColourTable.c
@@ -0,0 +1,40 @@
+/* ASCII Escape code text colour table */<
+
+#include <stdio.h><
+<
+main()<
+{<
+     int i, j;<
+     int colour[1];<
+<
+     printf("\n\n  256 Extended text colours\n\n");<
+<
+     for(i = 0; i <= 15; i++)<
+     {<
+          for(j = 0; j <= 15; j++)<
+          {<
+               colour[0] = i * 16 + j;<
+               printf("\x1b[38;5;%dm", colour[0]);
+               printf("%5d", i * 16 + j);<
+          }<
+          printf("\n");
+     }<
+     printf("\x1b[0m");               /* Reset */<
+     printf("\n");<I
+<
+     printf("\n  256 Extended Background colours\n\n");<
+<
+     for(i = 0; i <= 15; i++)<
+     {<
+          for(j = 0; j <= 15; j++)<
+          {<
+               colour[0] = i * 16 + j;<
+               printf("\x1b[48;5;%dm", colour[0]);<
+               printf("%5d", i * 16 + j);<
+          }<
+          printf("\x1b\[0m");<
+          printf("\n");<
+     }<
+     printf("\x1b[0m");               /* Reset */<
+     printf("\n");<
+}<
